e0ea4cd17184343b84e655d0a560cd2e4d838572
Petru Betco <petrubetco@gmail.com>
2019-07-01 15:58:49 +0300

    Added data to seed on DB creation

--- TechTask/TechTask.Persistence/EntityConfiguration/TeamConfiguration.cs
+++ TechTask/TechTask.Persistence/EntityConfiguration/TeamConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
 using TechTask.Persistence.Models.Users;
 
 namespace TechTask.Persistence.EntityConfiguration
@@ -11,6 +12,22 @@ namespace TechTask.Persistence.EntityConfiguration
             builder.HasKey(b => b.Id);
             builder.Property(b => b.Name).IsRequired().HasMaxLength(50);
             builder.Property(b => b.HoursOfWorkOnAllTasks).IsRequired();
+
+            builder.HasData(new List<Team>
+            {
+                new Team
+                {
+                    Id = 1,
+                    Name = "Alpha",
+                    HoursOfWorkOnAllTasks = 0
+                },
+                new Team
+                {
+                    Id = 2,
+                    Name = "Beta",
+                    HoursOfWorkOnAllTasks = 0
+                }
+            });
         }
     }
 }
--- TechTask/TechTask.Persistence/EntityConfiguration/UserConfiguration.cs
+++ TechTask/TechTask.Persistence/EntityConfiguration/UserConfiguration.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
 using TechTask.Persistence.Models.Users;
+using TechTask.Persistence.Models.Users.Enums;
 
 namespace TechTask.Persistence.EntityConfiguration
 {
@@ -15,6 +18,54 @@ namespace TechTask.Persistence.EntityConfiguration
             builder.Property(b => b.LastName).IsRequired().HasMaxLength(50);
             builder.Property(b => b.DateOfBirth).IsRequired();
             builder.Property(b => b.Role).IsRequired();
+
+            builder.HasData(new List<User>
+            {
+                new User
+                {
+                    Id = new Guid("bfd23b66-1a4e-41de-4aa7-08d6fae3b08b"),
+                    DateOfBirth = new DateTime(1984, 09, 13),
+                    FirstName = "Will",
+                    LastName = "Stevens",
+                    Email = "will.s@tech.com",
+                    Password = "Will123",
+                    Role = Roles.Admin,
+                    TeamId = 1
+                },
+                new User
+                {
+                    Id = new Guid("76aebd31-0235-4ef3-a123-08d6fbc1bdcd"),
+                    DateOfBirth = new DateTime(1993, 04, 28),
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Email = "john.s@tech.com",
+                    Password = "John123",
+                    Role = Roles.User,
+                    TeamId = null
+                },
+                new User
+                {
+                    Id = new Guid("ed09fe47-84c0-47b5-8007-ae2ea4350a8b"),
+                    DateOfBirth = new DateTime(1973, 10, 30),
+                    FirstName = "Jane",
+                    LastName = "Williams",
+                    Email = "jane.w@tech.com",
+                    Password = "Jane123",
+                    Role = Roles.Admin,
+                    TeamId = 2
+                },
+                new User
+                {
+                    Id = new Guid("f3c88d42-fb42-43c4-a9d4-1a738a2bd20c"),
+                    DateOfBirth = new DateTime(1988, 03, 31),
+                    FirstName = "Anthony",
+                    LastName = "Russell",
+                    Email = "anthony.r@tech.com",
+                    Password = "Anthony123",
+                    Role = Roles.User,
+                    TeamId = null
+                }
+            });
         }
     }
 }
