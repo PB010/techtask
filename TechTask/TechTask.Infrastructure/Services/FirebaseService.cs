3ec0304849eb1ea7ed4373259ae81f83729320a6
Petru Betco <petrubetco@gmail.com>
2019-07-08 22:28:45 +0300

    Trying to implement firebase api

---
+++ TechTask/TechTask.Infrastructure/Services/FirebaseService.cs
@@ -0,0 +1,16 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+namespace TechTask.Infrastructure.Services
+{
+    public class FirebaseService
+    {
+        public FirebaseService()
+        {
+            FirebaseApp.Create(new AppOptions
+            {
+                Credential = GoogleCredential.GetApplicationDefault()
+            });
+        }
+    }
+}
